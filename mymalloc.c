556f747c4aadb25cf4c391dadb1b0d96f07cd681
sgogolowski <38380423+sgogolowski@users.noreply.github.com>
2019-10-08 12:37:48 -0400

    Add files via upload

---
+++ mymalloc.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "MYMALLOC_H.h"
+
+
+typedef struct node{
+	
+  int sizeOfBytes;
+  struct Node* next;
+  int isEmpty;
+}Node;
+Node* head;
+head->sizeOfBytes=4080;
+head->next=NULL;
+head-> isEmpty=1;
+
+
+void* mymalloc(size_t size, __FILE__, __LINE__){
+  if(size>4080)
+    printf("You are asking for too much memory at file: %s line %d\n",__FILE__,__LINE__);		
+  Node* availableSpace=findSpace(size);
+  if(availableSpace == NULL){
+    printf("There is not space available for your %d bytes\n",size);
+  }
+  else{
+    splitMemory(availableSpace);
+    
+    
+    
+  }
+
+
+
+
+}
+
+void splitMemory(Node* memory,size_t sizeRequested){
+  Node* freeMem={
+    sizeOfBytes=(memory->sizeOfBytes)-sizeRequested;
+    isEmpty=1;
+    next=memory->next->next;
+  }
+  memory->sizeOfBytes=sizeRequested;
+  memory->isEmpty=0;
+  memory->next=freeMem;
+  
+  
+  
+}
+
+void coalesceMemory(){
+  Node* lastFull, lastEmpty;
+  Node current=
+
+
+
+}
+
+Node* findSpace(size_t sizeRequested){
+  Node* current=Node;
+  while(current != NULL){
+  
+    if(current->isEmpty==0 && current->sizeOfBytes >= (sizeRequested + 16)){
+      return current;
+      
+    }
+  current=current->next;
+  }
+  return NULL;
+
+
+}
+
+
+
+
+
+
---
+++ mymalloc.h
@@ -0,0 +1,19 @@
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+
+#include <stdio.h>
+#include <stlib.h>
+
+#define MAXSIZE 4096
+#define free(x) myfree(x, __FILE__, __LINE__)
+#define malloc(x) mymalloc(x, __FILE__ , __LINE__)
+
+static char meymemory[4096];
+
+void* mymalloc(size_t size, char* file, int line);
+
+void myfree(void* ptr), char* file, int line);
+
+
+
+#endif
